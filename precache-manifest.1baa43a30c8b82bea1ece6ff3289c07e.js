e6ac82c780f2854979cbfb7d6dc77c56207a5573
= <=>
2020-09-28 00:58:08 +0530

    Updates

---
+++ precache-manifest.1baa43a30c8b82bea1ece6ff3289c07e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f0c441a90b30cf199c9413ca67f95f",
+    "url": "/eventManager/index.html"
+  },
+  {
+    "revision": "85bb3c7b9735d9aadda7",
+    "url": "/eventManager/static/css/main.350006c0.chunk.css"
+  },
+  {
+    "revision": "a1897dd502a48a01cf7c",
+    "url": "/eventManager/static/js/2.a2ead49c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/eventManager/static/js/2.a2ead49c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85bb3c7b9735d9aadda7",
+    "url": "/eventManager/static/js/main.e845c95d.chunk.js"
+  },
+  {
+    "revision": "21fae92e3887753b50c2",
+    "url": "/eventManager/static/js/runtime-main.61d3a969.js"
+  }
+]);
